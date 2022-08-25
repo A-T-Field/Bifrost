1afcef3897690b821b8686d2dcbb6a8bad31ac56
maggot_code <wowhuanhuan233@163.com>
2022-08-25 18:14:21 +0800

    Update node.test.ts

--- pkg/Tree/Node/__test__/node.test.ts
+++ pkg/Tree/Node/__test__/node.test.ts
@@ -3,7 +3,7 @@
  * @Author: maggot-code
  * @Date: 2022-08-25 16:49:43
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-08-25 18:08:54
+ * @LastEditTime: 2022-08-25 18:14:14
  * @Description:
  */
 import TestData from "@pkg/Tree/json/v1.test.json";
@@ -14,6 +14,10 @@ const id = v4();
 
 describe("Node", () => {
     it("test", () => {
+        const { UAParser } = require("ua-parser-js");
+        const ua = UAParser(navigator.userAgent);
+        console.log(ua);
+
         console.log(TestData);
         console.log(id);
         console.log(NIL);
