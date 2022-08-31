0c96b4d7cabeed77647de3782db648bf1581fcd1
maggot_code <wowhuanhuan233@163.com>
2022-08-31 18:05:40 +0800

    fix :ambulance:

--- package/shared/uuid/index.ts
+++ package/shared/uuid/index.ts
@@ -3,18 +3,27 @@
  * @Author: maggot-code
  * @Date: 2022-08-31 16:03:46
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-08-31 17:12:12
+ * @LastEditTime: 2022-08-31 18:05:00
  * @Description:
  */
 import { v4 as uuidv4 } from "uuid";
 
 export type Key = string | number;
+export type ToEqual = (uuid: UUID) => boolean;
+export abstract class UUID {
+    readonly value: Key;
+    public toEqual: ToEqual;
+}
 
-export function useUUID() {
+export function useUUID(): UUID {
     const value: Key = uuidv4();
+    const toEqual: ToEqual = (uuid) => {
+        return uuid.value === value;
+    };
 
     return {
         value,
+        toEqual,
     };
 }
 
