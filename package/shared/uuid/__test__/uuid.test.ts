c2d11e34344d93e31f91799e22a81f5893f18be5
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-09-02 00:41:06 +0800

    Update uuid.test.ts

--- package/shared/uuid/__test__/uuid.test.ts
+++ package/shared/uuid/__test__/uuid.test.ts
@@ -3,7 +3,7 @@
  * @Author: maggot-code
  * @Date: 2022-09-01 23:42:05
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-09-02 00:38:32
+ * @LastEditTime: 2022-09-02 00:41:03
  * @Description: 
  */
 import {
@@ -21,20 +21,25 @@ describe("UUID", () => {
     test("log", () => {
         console.log(uid);
     });
+
     test("is uuid", () => {
         expect(isUUID(uid)).toBe(true);
     });
+
     test("not uuid", () => {
         expect(notUUID(uid)).toBe(false);
     });
+
     test("has uuid", () => {
         const [value,error] = hasUUID(uid);
         expect(value).toBe(uid.value);
         expect(error).toBe(true);
     });
+
     test("to clean", () => {
         expect(toClean(uid)).toBe(uid.value.split("-").join(""));
     });
+    
     test("to format", () => {
         expect(toFormat(uid)).toBe(uid.value.replace(/^(.{8})(.{4})(.{4})(.{4})(.{12})$/, "$1-$2-$3-$4-$5"));
     });
