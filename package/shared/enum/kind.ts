d340b17d3e7f7c54a7798646151ec2bb4e317931
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-09-02 00:40:32 +0800

    fix :-1:

---
+++ package/shared/enum/kind.ts
@@ -0,0 +1,11 @@
+/*
+ * @FilePath: /Bifrost/package/shared/enum/kind.ts
+ * @Author: maggot-code
+ * @Date: 2022-09-02 00:09:02
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-09-02 00:12:00
+ * @Description: 
+ */
+export enum Common {
+    UUIDV4 = 0
+}
